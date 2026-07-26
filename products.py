076889e7f8fd4d955ef55adf43f388c646945dde
jai singh <ks2007851@gmail.com>
2026-07-26 15:00:55 +0530

    Add Adjustable Monitor Arm to products catalog

--- products.py
+++ products.py
@@ -13,4 +13,5 @@ products = [
     {"id": 11, "name": "Bluetooth Speaker", "price": 2199},
     {"id": 12, "name": "Noise Cancelling Earbuds", "price": 3499},
     {"id": 13, "name": "Mechanical Gaming Mouse", "price": 1899},
+    {"id": 14, "name": "Adjustable Monitor Arm", "price": 3299},
 ]
