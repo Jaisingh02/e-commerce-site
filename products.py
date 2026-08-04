eaf5bb1980591df75b862120e26432420231d176
jai singh <ks2007851@gmail.com>
2026-08-04 23:47:06 +0530

    Add Desk Fan USB Powered to products catalog

--- products.py
+++ products.py
@@ -20,4 +20,5 @@ products = [
     {"id": 18, "name": "Cable Management Sleeve", "price": 499},
     {"id": 19, "name": "USB Microphone Kit", "price": 2799},
     {"id": 20, "name": "Portable Monitor 15.6 Inch", "price": 8999},
+    {"id": 21, "name": "Desk Fan USB Powered", "price": 799},
 ]
