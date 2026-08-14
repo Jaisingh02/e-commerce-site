860e0d72bb56e7e41d8fc26f10f4cf7bf6630010
jai singh <ks2007851@gmail.com>
2026-08-14 09:49:52 +0530

    Add Desk Organizer Tray to products catalog

--- products.py
+++ products.py
@@ -32,4 +32,5 @@ products = [
     {"id": 30, "name": "USB Desk Fan Oscillating", "price": 1199},
     {"id": 31, "name": "Wireless Bluetooth Earbuds Pro", "price": 4999},
     {"id": 32, "name": "Mechanical Keycaps Set", "price": 1299},
+    {"id": 33, "name": "Desk Organizer Tray", "price": 899},
 ]
