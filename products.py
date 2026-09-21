7d2496d2a346a0ded945aafe9f383c4d4a8e475e
jai singh <ks2007851@gmail.com>
2026-09-21 21:30:12 +0530

    Add Noise Cancelling Microphone Sponge to products catalog

--- products.py
+++ products.py
@@ -51,4 +51,5 @@ products = [
     {"id": 49, "name": "Wireless Touchpad Remote", "price": 1499},
     {"id": 50, "name": "Ergonomic Footrest Cushion", "price": 1899},
     {"id": 51, "name": "Anti-Glare Screen Protector", "price": 799},
+    {"id": 52, "name": "Noise Cancelling Microphone Sponge", "price": 299},
 ]
