e36b68387825095443468bff6a0ff54f2c5b734f
jai singh <ks2007851@gmail.com>
2026-09-22 21:47:37 +0530

    Add USB-C Angled Adapter 90-Degree to products catalog

--- products.py
+++ products.py
@@ -58,4 +58,5 @@ products = [
     {"id": 56, "name": "RGB Mechanical Numpad", "price": 1799},
     {"id": 57, "name": "Adjustable Tablet Stand Holder", "price": 899},
     {"id": 58, "name": "Magnetic Smartphone Ring Holder", "price": 399},
+    {"id": 59, "name": "USB-C Angled Adapter 90-Degree", "price": 299},
 ]
