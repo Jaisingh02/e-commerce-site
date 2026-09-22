06a88349e98d7833b676e603a96238324790d79a
jai singh <ks2007851@gmail.com>
2026-09-22 22:05:16 +0530

    Add Cable Management Sleeve Neoprene to products catalog

--- products.py
+++ products.py
@@ -65,4 +65,5 @@ products = [
     {"id": 63, "name": "Anti-Static Wrist Strap", "price": 249},
     {"id": 64, "name": "Precision Screwdriver Tool Kit 64-in-1", "price": 1499},
     {"id": 65, "name": "Cable Tie Rolls 50-Pack", "price": 399},
+    {"id": 66, "name": "Cable Management Sleeve Neoprene", "price": 699},
 ]
